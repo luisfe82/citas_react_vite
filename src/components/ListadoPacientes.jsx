0bff311707a329024df9cc385ce5889ae8e31865
Luisfe82 <luisfediana@gmail.com>
2023-03-25 21:58:41 -0600

    actualizo algo sencillo

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -8,7 +8,7 @@ const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente}) => {
                 <>
                     <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
                     <p className="text-xl mt-5 mb-10 text-center">
-                        Administra tus {''}
+                        Administra Tus {''}
                         <span className="text-indigo-600 font-bold ">Pacientes y Citas</span>
                     </p>
 
